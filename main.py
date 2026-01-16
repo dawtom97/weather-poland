dcc3da560575358b71a0b083b9736a008e8d7443
dawtom97 <dawtom97@gmail.com>
2026-01-16 20:38:15 +0100

    fetch weather service

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from config import Config
+from services.openweather_api import fetch_weather
+import time
 
-print(Config.OPENWEATHER_API_KEY)
-print(Config.OPENWEATHER_CITY)
+while True:
+    weather = fetch_weather()
+    print(weather)
+    # Zapis do excel / csv
+    # Zapis do DB
+    # Wykresy i interfejs
+    time.sleep(60)
 
