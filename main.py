75d3bc1460bb1834b42efe7c8edde810ddd31e1a
dawtom97 <dawtom97@gmail.com>
2026-01-26 20:50:17 +0100

    mysql connection

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from config import Config
 from services.openweather_api import fetch_weather
+from services.excel_files import save_to_excel,read_excel_file
+from services.mysql_db import save_record
+from config import Config
 import time
 
 while True:
     weather = fetch_weather()
-    print(weather)
-    # Zapis do excel / csv
+    # save_to_excel(weather, Config.XLSX_PATH)
+    # read_excel_file(Config.XLSX_PATH)
+    save_record(weather)
     # Zapis do DB
     # Wykresy i interfejs
-    time.sleep(60)
+    time.sleep(5)
 
